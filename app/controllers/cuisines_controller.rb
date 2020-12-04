5dac8336d8a0146c72489baf342d9f782816346c
Lois K. Lee <lois.lee@me.com>
2020-12-04 00:18:11 -0500

    retrieve api-key via header; get hardcode response

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,4 +1,14 @@
+require 'httparty'
+require 'json'
+
 class CuisinesController < ApplicationController
+  
   def index
+    @key = request.headers["user-key"]
+    response = HTTParty.get("https://developers.zomato.com/api/v2.1/cities?q=Princeton", headers: {"Accept" => "application/JSON", "user-key" => "#{@key}"}).to_s
+    @parsed_response = JSON.parse(response)
+    
+    render :json => @parsed_response
   end
+
 end
