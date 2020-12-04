bbe79c4c8a5cee4607afed00842b76b346bc71a0
Lois K. Lee <lois.lee@me.com>
2020-12-04 01:31:24 -0500

    retrieve reviews for cities 2 & 3

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
   
   def index
     @key = request.headers["user-key"]
-    response = HTTParty.get("https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&cuisines=25", headers: {"Accept" => "application/JSON", "user-key" => "#{@key}"}).to_s
+    response = HTTParty.get("https://developers.zomato.com/api/v2.1/search?entity_id=#{params[:city_id]}&entity_type=city&cuisines=#{params[:cuisine_id]}", headers: {"Accept" => "application/JSON", "user-key" => "#{@key}"}).to_s
 
     @parsed_response = JSON.parse(response)
     city_1 = @parsed_response["restaurants"][0]["restaurant"]["R"]["res_id"]
@@ -13,9 +13,15 @@ class ReviewsController < ApplicationController
     @rev = JSON.parse(revres)
     @views1 =  @rev["user_reviews"]
 
+    revres2 = HTTParty.get("https://developers.zomato.com/api/v2.1/reviews?res_id=#{city_2}", headers: {"Accept" => "application/JSON", "user-key" => "#{@key}"}).to_s
+    @rev2 = JSON.parse(revres2)
+    @views2 =  @rev2["user_reviews"]
 
+    revres3 = HTTParty.get("https://developers.zomato.com/api/v2.1/reviews?res_id=#{city_3}", headers: {"Accept" => "application/JSON", "user-key" => "#{@key}"}).to_s
+    @rev3 = JSON.parse(revres3)
+    @views3 =  @rev3["user_reviews"]
 
-    render :json => @views1
+    render :json => @views3
 
   end
 end
