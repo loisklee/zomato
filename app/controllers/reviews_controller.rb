7f6ba53476408cb324124a07e2ca47d9a4412dc8
Lois K. Lee <lois.lee@me.com>
2020-12-04 01:01:53 -0500

    create reviews route, get hardcode search response

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+  
+  def index
+    @key = request.headers["user-key"]
+    response = HTTParty.get("https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&cuisines=25", headers: {"Accept" => "application/JSON", "user-key" => "#{@key}"}).to_s
+
+    @parsed_response = JSON.parse(response)
+    render :json => @parsed_response
+
+  end
 end
