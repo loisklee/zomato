07b46870e7df8f6ce9b6611499f2516fb1e16002
Lois K. Lee <lois.lee@me.com>
2020-12-04 00:20:11 -0500

    save city_id & city_info

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -6,8 +6,11 @@ class CuisinesController < ApplicationController
   def index
     @key = request.headers["user-key"]
     response = HTTParty.get("https://developers.zomato.com/api/v2.1/cities?q=Princeton", headers: {"Accept" => "application/JSON", "user-key" => "#{@key}"}).to_s
-    @parsed_response = JSON.parse(response)
     
+    @parsed_response = JSON.parse(response)
+    @city_id = @parsed_response["location_suggestions"][0]["id"]
+    @city_info = @parsed_response["location_suggestions"][0]
+
     render :json => @parsed_response
   end
 
