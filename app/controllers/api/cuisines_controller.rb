35b153f9e6a518545e26d4179e1604a761364ea8
Lois K. Lee <lois.lee@me.com>
2020-12-18 16:11:02 -0500

    take out requires and put error before request

--- app/controllers/api/cuisines_controller.rb
+++ app/controllers/api/cuisines_controller.rb
@@ -1,18 +1,16 @@
-require 'httparty'
-require 'json'
-
 class Api::CuisinesController < ApplicationController
   def index
     key = request.headers["user-key"]
-    response = JSON.parse(HTTParty.get("https://developers.zomato.com/api/v2.1/cities?q=#{params[:city]}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"}).to_s)
-    
-    return render json: 'Error: Missing API key', status: 401 unless key
-    return render json: 'Error: Missing city query', status: 400 unless params[:city]
 
+    return render json: 'Error: Missing API key', status: 401 unless !key.empty?
+    return render json: 'Error: Missing city query', status: 400 unless !params[:city].empty?
+
+    response = HTTParty.get("https://developers.zomato.com/api/v2.1/cities?q=#{params[:city]}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"})
+    
     city_id = response["location_suggestions"][0]["id"]
     city_info = response["location_suggestions"][0]
 
-    sec_response = JSON.parse(HTTParty.get("https://developers.zomato.com/api/v2.1/cuisines?city_id=#{city_id}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"}).to_s)
+    sec_response = HTTParty.get("https://developers.zomato.com/api/v2.1/cuisines?city_id=#{city_id}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"})
 
     city_info['cuisines'] = sec_response["cuisines"].map do |cuisine|
       {
--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,25 +1,24 @@
-require 'httparty'
-require 'json'
-
 class Api::ReviewsController < ApplicationController
   def index
     key = request.headers["user-key"]
-    response = JSON.parse(HTTParty.get("https://developers.zomato.com/api/v2.1/search?entity_id=#{params[:city_id]}&entity_type=city&cuisines=#{params[:cuisine_id]}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"}).to_s)
 
-    return render json: 'Error: Missing API key', status: 401 unless key
-    return render json: 'Error: Missing city ID and/or cuisine ID', status: 400 if !params[:city_id] || !params[:cuisine_id]
+    return render json: 'Error: Missing API key', status: 401 unless !key.empty?
+    return render json: 'Error: Missing city ID', status: 400 unless !params[:city_id].empty?
+    return render json: 'Error: Missing cuisine ID', status: 400 unless !params[:cuisine_id].empty?
+
+    response = HTTParty.get("https://developers.zomato.com/api/v2.1/search?entity_id=#{params[:city_id]}&entity_type=city&cuisines=#{params[:cuisine_id]}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"})
 
     city_1 = response["restaurants"][0]["restaurant"]["R"]["res_id"]
     city_2 = response["restaurants"][1]["restaurant"]["R"]["res_id"]
     city_3 = response["restaurants"][2]["restaurant"]["R"]["res_id"]
 
-    revres = JSON.parse(HTTParty.get("https://developers.zomato.com/api/v2.1/reviews?res_id=#{city_1}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"}).to_s)
+    revres = HTTParty.get("https://developers.zomato.com/api/v2.1/reviews?res_id=#{city_1}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"})
     views1 =  revres["user_reviews"]
 
-    revres2 = JSON.parse(HTTParty.get("https://developers.zomato.com/api/v2.1/reviews?res_id=#{city_2}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"}).to_s)
+    revres2 = HTTParty.get("https://developers.zomato.com/api/v2.1/reviews?res_id=#{city_2}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"})
     views2 =  revres2["user_reviews"]
 
-    revres3 = JSON.parse(HTTParty.get("https://developers.zomato.com/api/v2.1/reviews?res_id=#{city_3}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"}).to_s)
+    revres3 = HTTParty.get("https://developers.zomato.com/api/v2.1/reviews?res_id=#{city_3}", headers: {"Accept" => "application/JSON", "user-key" => "#{key}"})
     views3 =  revres3["user_reviews"]
 
     final = (views1).concat(views2).concat(views3)
